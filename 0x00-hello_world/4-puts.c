3a110f2734c855a8a6f2e4cc30607c853c008ea4
Chalachew Yisihak <chalachew.yisihak22@gmail.com>
2022-10-14 12:41:47 +0000

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-	char result[20] ="\"Programming is like building a multilingual puzzle";
+	char result[51] ="\"Programming is like building a multilingual puzzle";
 
 	puts(result);
 	return (0);

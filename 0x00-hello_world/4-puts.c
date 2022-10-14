6548e453b4b6f725b58b9f717c6f3f6d0345fbd1
Chalachew Yisihak <chalachew.yisihak22@gmail.com>
2022-10-14 13:23:06 +0000

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+*main - Entry point
+*
+*Return:0
+*
+*/
 int main(void)
 {
 	char result[52] = "\"Programming is like building a multilingual puzzle";

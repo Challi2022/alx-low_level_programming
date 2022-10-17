47d5dffd11f39846a3f8049537131e95d16e4c10
Chalachew Yisihak <chalachew.yisihak22@gmail.com>
2022-10-17 09:24:34 +0000

    all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,32 +1,37 @@
 #include <stdio.h>
 /**
 *main-program entry point.
-*
-*Return:0 success, non zero error.
+*Return:0 success, non zero fail
 **/
 int main(void)
 {
-	int i;
-	int j;
+	int i, k = '0', j;
 
 	for (i = '0'; i <= '9'; i++)
 	{
-
-		for (j = '1'; j <= 9 ; j++)
+		for (j = k; j <= '9'; j++)
 		{
-			if (i < j)
+			if (i != j)
 			{
-			putchar(i);
-			putchar(j);
-			putchar(',');
-			putchar(' ');
+				putchar(i);
+				putchar(j);
 			}
-			else
+			if (i == j)
+			{
+				continue;
+			}
+			if (i == '8' && j == '9')
 			{
 				break;
 			}
-
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
+		k++;
 	}
+	putchar('\n');
 	return (0);
 }

2edd5e8283815c6cb08f0d3c026d41b4610c037c
Chalachew Yisihak <chalachew.yisihak22@gmail.com>
2022-10-18 14:09:07 +0000

    Putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+#define _purchar
+int _putchar(void);
+/**
+ *main-program entry point.
+ *
+ *Return: On success 0.
+ **/
+int main(void)
+{
+
+	return (0);
+
+}
+/**
+ *_putchar - writes the character to stdout
+ *
+ *Return: On success 1.
+ *On error, -1 is returned, and errno is set appropriately.
+ **/
+int _putchar(void)
+{
+	putchar ('_');
+	putchar ('p');
+	putchar ('u');
+	putchar ('t');
+	putchar ('c');
+	putchar ('h');
+	putchar ('a');
+	putchar ('r');
+	putchar ('\n');
+	return (1);
+}

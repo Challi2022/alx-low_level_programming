02937c67507d73686107f5ccdc8f5d8f5eb17def
Chalachew Yisihak <chalachew.yisihak22@gmail.com>
2022-10-19 13:37:56 +0000

    Commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -27,16 +27,17 @@ void print_to_98(int n)
 			n++;
 		}
 
-		else
+	else
+
+	{
+		while (n >= 98)
 		{
-			while (n >= 98)
-			{
-				_putchar(n);
-				_putchar(',');
-				_putchar(' ');
-			}
-			n--;
+			_putchar(n);
+			_putchar(',');
+			_putchar(' ');
 		}
-		_putchar('\n');
+		n--;
+	}
+	_putchar('\n');
 	}
 }

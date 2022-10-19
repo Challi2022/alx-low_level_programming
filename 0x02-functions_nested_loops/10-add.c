1ced1d8f780fcfc1d0dce3620272bb9e8ae8cd1e
Chalachew Yisihak <chalachew.yisihak22@gmail.com>
2022-10-19 13:08:34 +0000

    commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *add - add two ingigers
+ *@a: numbers to be added
+ *@b: number to be added
+ *Return: result
+ **/
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
+

5f01e9207345b3271065ea1f893d1126cbfce905
Chalachew Yisihak <chalachew.yisihak22@gmail.com>
2022-10-19 13:31:50 +0000

    Commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ *print_to_98 - prints all natural numbers from n to 98
+ *@n: the number to be passed to the function
+ *Rerurn: 0
+ **/
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n <= 98)
+		{
+			if (n < 0)
+			{
+				_putchar('-');
+				_putchar(n);
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(n);
+				_putchar(',');
+				_putchar(' ');
+			}
+			n++;
+		}
+
+		else
+		{
+			while (n >= 98)
+			{
+				_putchar(n);
+				_putchar(',');
+				_putchar(' ');
+			}
+			n--;
+		}
+		_putchar('\n');
+	}
+}

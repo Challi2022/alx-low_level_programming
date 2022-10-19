f4a6403e677e4f87d9e5a55edf1adf182d4fb7ae
Chalachew Yisihak <chalachew.yisihak22@gmail.com>
2022-10-19 06:58:07 +0000

    task 0

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,38 +1,18 @@
-#include <stdio.h>
-<<<<<<< HEAD
+#include "main.h"
 /**
- *print_alphabet-program entry point.
- *
- *Reurun: On success 0.
+ *main-program entry point
+ *.
+ *Reurun:0
  **/
-void print_alphabet(void)
-{
-	char c = 'a';
-
-	while (c <= 'z')
-	{
-		putchar('c');
-		c++;
-	}
-	putchar('\n');
-	return;
-=======
-void print_alphabet(void);
-/**
- *main-program entry point.
- *
- *Return: On success 0.
- **/
-
 int main(void)
 {
 	char ch = 'a';
 
 	while (ch <= 'z')
 	{
-	ch++;
+		_putchar('ch');
+	h++;
 	}
-	putchar('\n');
+	_putchar('\n');
 	return (0);
->>>>>>> 0f370660b9002c77f5f01e2bde6ed62b81c9dd79
 }

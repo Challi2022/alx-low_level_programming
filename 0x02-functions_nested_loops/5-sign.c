3b955c840031d1a4999ca6b9fcac3762f000f40d
Chalachew Yisihak <chalachew.yisihak22@gmail.com>
2022-10-19 08:53:09 +0000

    Commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  *print_sign - prints the sign of a number.
  *@n: is the letter to be checked
@@ -11,11 +12,13 @@ int print_sign(int n)
 		_purchar('+');
 		return (1);
 	}
+
 	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
 	}
+
 	else
 	{
 		_putchar('-');

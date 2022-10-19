d2035cdb99a5245852914986daa39ec7bcab0044
Chalachew Yisihak <chalachew.yisihak22@gmail.com>
2022-10-19 08:33:51 +0000

    Commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *print_sign - prints the sign of a number.
+ *@n: is the letter to be checked
+ *Return:1 if positive,-1 if negative, otehrwise 0.
+ **/
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	else if (n == 0)
+		return (0);
+	else
+		return (0);
+}

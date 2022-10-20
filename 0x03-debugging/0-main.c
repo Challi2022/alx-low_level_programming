37736bff900618186cc4ed4eced88ce6d36f833c
Chalachew Yisihak <chalachew.yisihak22@gmail.com>
2022-10-20 10:38:24 +0000

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,25 @@
-#include "main.h"
-
+#include <stdio.h>
 /**
- *main - tests function that prints if integer is positive or negative
- *Return: 0
+ *main - Entry point
+ *
+ *Return: return 0: no error, non zero alue for error
  **/
-
 int main(void)
 {
-	int i;
+	int n;
 
-	i = 98;
-	positive_or_negative(i);
+	positive_or_negative(int i);
+	i = rand() - RAND_MAX / 2;
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else
+		printf("%d is zero\n", i);
 
 	return (0);
 }

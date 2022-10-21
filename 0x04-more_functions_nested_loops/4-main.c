c40056501fd27fb11fdbfde1b128002aa9e504ec
Chalachew Yisihak <chalachew.yisihak22@gmail.com>
2022-10-21 18:13:56 +0000

    Print 14 times

---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	int num;
+
+	num = print_most_numbers();
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+* more_numbers - check the code
+*
+* Return: Always 0.
+*/
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		int j;
+
+		for (j = 0; j < 14; j++)
+		{
+			_putchar('i');
+		}
+
+		_putchar('\n');
+	}
+}

d56130f9c2d71d37e608fdca7feaca0d268a5893
Chalachew Yisihak <chalachew.yisihak22@gmail.com>
2022-10-21 22:15:53 +0000

    Square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  *print_square - Prints the square # of a given number
+  *@size: is the number to be checked
+  *
+  **/
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+		_putchar('\n');
+
+	}
+
+	else
+		_putchar('\n');
+}

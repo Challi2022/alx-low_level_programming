9afeafd9b10be95bb6f3cb7aea37bce783ea0f9a
Chalachew Yisihak <chalachew.yisihak22@gmail.com>
2022-10-21 18:54:12 +0000

    Prints diagonal

---
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+   * main - check the code
+    *
+     * Return: Always 0.
+      */
+int main(void)
+{
+	int n;
+
+	print_diagonal(n);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*print_diagonal - prints diagonal of a given number
+*@n: number to be chedked
+**/
+
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		int i;
+
+		for (i = 0; i <= n; i++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\');
+	}
+	else
+		_putchar('\n');
+
+}

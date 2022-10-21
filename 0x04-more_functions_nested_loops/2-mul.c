3aa6278e3b9aa7bb5c5bf5d548414c5ae6af89c0
Chalachew Yisihak <chalachew.yisihak22@gmail.com>
2022-10-21 12:31:29 +0000

    Multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,13 +3,9 @@
   *mul - check the lowercase charactor.
   *@a:number to be multiplied
   *@b:number to be multiplied
-  *@m: The multiplication result
-  *Return:0.
+  *Return:value
   **/
 int mul(int a, int b)
 {
-	int m;
-
-	m = (a * b);
-	return (m);
+	return (a * b);
 }

456abf5c70a32e8b241f297872c2fb227eebf892
Chalachew Yisihak <chalachew.yisihak22@gmail.com>
2022-10-21 18:16:01 +0000

    Print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
 *print_most_numbers  - prints numbers from 0 to 9.
-*@i: is the number to be checked
 **/
 
 void print_most_numbers(void)
 {
-	for (int i = 0; i <= 9; i++)
+	int i;
+
+	for (i = 0; i <= 9; i++)
 	{
 		if (i == 2 && i == 4)
 			continue;

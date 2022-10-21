9984f1590e7d3bd8786c26bc7a1ab1e12598b4c3
Chalachew Yisihak <chalachew.yisihak22@gmail.com>
2022-10-21 18:33:50 +0000

    Print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_line - prints line.
+*
+*@n: number to be checked.
+*/
+
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		while (n > 0)
+		{
+			_putchar('_');
+		}
+		n--;
+	else
+		_putchar('\n');
+	}
+}

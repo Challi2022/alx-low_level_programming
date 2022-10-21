e740b4c469432984c222ac70d874a7d20ec1838c
Chalachew Yisihak <chalachew.yisihak22@gmail.com>
2022-10-21 13:20:37 +0000

    Print 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,16 +6,12 @@
 
 void print_numbers(void)
 {
+	int i = '0';
 
-	_putchar('0');
-	_putchar('1');
-	_putchar('2');
-	_putchar('3');
-	_putchar('4');
-	_putchar('5');
-	_putchar('6');
-	_putchar('7');
-	_putchar('8');
-	_putchar('9');
+	while (i <= '9')
+	{
+	putchar('i');
+	}
+	i++;
 	_putchar('\n');
 }

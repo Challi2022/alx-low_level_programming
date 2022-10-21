f5eed01febb54fc134bfb63e3efe39c4edf39c5b
Chalachew Yisihak <chalachew.yisihak22@gmail.com>
2022-10-21 12:49:57 +0000

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_numbers - prints numbers from 0 to 9.
+*@i: is the number to be checked
+*Return: 0
+**/
+
+void print_numbers(void)
+{
+
+	for (int i = 0; i <= 9; i++)
+		_putchar(i);
+
+	_putchar('\n');
+	return (0);
+}

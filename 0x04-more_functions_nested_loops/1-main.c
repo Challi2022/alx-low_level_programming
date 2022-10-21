322e7e99105e593d40ebdd6fb2c0a48714112a3f
Chalachew Yisihak <chalachew.yisihak22@gmail.com>
2022-10-21 11:18:55 +0000

    Is Digit

---
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	c = '0';
+	_isdigit(c);
+	c = 'a';
+	_isdigit(c);
+
+	return (0);
+}

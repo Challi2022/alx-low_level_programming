96b52ee0f511f1ee332bfe4c4c970ab4fef873c4
Chalachew Yisihak <chalachew.yisihak22@gmail.com>
2022-10-21 11:32:24 +0000

    Multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  *mul - check the lowercase charactor.
+  *@res: is the charachtor to be checked
+  *@a:number to be multiplied
+  *@b:number to be multiplied
+  *Return: res.
+  **/
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+	return (res);
+}

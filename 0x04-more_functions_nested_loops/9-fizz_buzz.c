34282b4e765e07335864cdc7a54c4d6066035734
Chalachew Yisihak <chalachew.yisihak22@gmail.com>
2022-10-22 19:17:58 +0000

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,37 +14,17 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 	if (i % 3 == 0 && i % 5 == 0)
-	{
-		_putchar('F');
-		_putchar('i');
-		_putchar('z');
-		_putchar('z');
-		_putchar('B');
-		_putchar('u');
-		_putchar('z');
-		_putchar('z');
-		_putchar(' ');
-	}
+		printf("FizzBuzz ");
 	else if (i % 3 == 0)
-	{
-		_putchar('F');
-		_putchar('i');
-		_putchar('z');
-		_putchar('z');
-		_putchar(' ');
-	}
+
+		printf("Fizz ");
+
 	else if (i % 5 == 0)
-	{
-		_putchar('B');
-		_putchar('u');
-		_putchar('z');
-		_putchar('z');
-		_putchar(' ');
-	}
+
+		printf("Buzz ");
 	else
-	{	_putchar(i);
-		_putchar(' ');
+		printf("%d ", i);
 	}
-	}
+	printf("\n");
 	return(0);
 }

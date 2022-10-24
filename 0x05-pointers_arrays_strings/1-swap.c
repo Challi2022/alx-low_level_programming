3990d48c6d56746735d196d0223468303b92cbdf
Chalachew Yisihak <chalachew.yisihak22@gmail.com>
2022-10-24 19:54:54 +0000

    Swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *swap_int - swaps the value of two intigers
+ *@a: is the value to be swapped
+ *@b: the value to be swapped
+ **/
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}

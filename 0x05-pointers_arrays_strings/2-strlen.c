48f350cfb331b4fbd9689c5a00b7e14208103205
Chalachew Yisihak <chalachew.yisihak22@gmail.com>
2022-10-25 08:09:18 +0000

    String length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
 *_strlen - returns string length.
 * @s: string to be cheched
@@ -7,9 +8,5 @@
 
 int _strlen(char *s)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-		
-	return (strlen());
+	return (strlen(s));
 }

723a0e40afcd5367b77e756c7d940ab54b0c9a3a
Chalachew Yisihak <chalachew.yisihak22@gmail.com>
2022-10-25 06:39:10 +0000

    swap

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_strlen - returns string length.
+* @s: string to be cheched
+* Return: 0 for success
+**/
+
+int _strlen(char *s)
+{
+	int *length;
+
+	length = strlen(s);
+	return (length);
+}

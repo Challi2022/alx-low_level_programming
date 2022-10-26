e855334f10085da0338b9a78cb7789601e30224e
Chalachew Yisihak <chalachew.yisihak22@gmail.com>
2022-10-26 12:19:18 +0000

    prints n elements of an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,9 +11,14 @@ void print_array(int *a, int n)
 
 	while (i < n)
 	{
-		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf("%d", a[i]);
+			printf(", ");
+		}
+		else
+			printf("%d", a[i]);
 		i++;
-		printf(", ");
 	}
 
 	printf("\n");

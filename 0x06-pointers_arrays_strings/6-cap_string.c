07441ec8682b105c45a2205b6fc48d60936a36de
Chalachew Yisihak <chalachew.yisihak22@gmail.com>
2022-10-27 09:47:48 +0000

    Capitalization

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,6 +2,15 @@
 #include <stdio.h>
 #include <string.h>
 /**
+*capital - Checks for letter passed
+*@c: a character to be checked
+**/
+void capital(char *c)
+{
+	if (*c >= 'a' && *c <= 'z')
+		*c = *c - 32;
+}
+/**
 *cap_string - A string to be capitalized
 *@s: a string value passed
 *Return: value
@@ -14,16 +23,27 @@ char *cap_string(char *s)
 	{
 		if (i == 0)
 		{
-			if (s[i] >= 'a' && s[i] <= 'z')
-				s[i] = s[i] - 32;
-			continue;
+			capital(&s[i]);
 		}
-		if (s[i]  == ' ')
+		else if (s[i - 1] == ' ' || s[i - 1] == '\n' || s[i - 1] == ',')
 		{
-			++i;
-			if (s[i] >= 'a' && s[i] <= 'z')
-				s[i] = s[i] - 32;
-			continue;
+			capital(&s[i]);
+		}
+		else if (s[i - i] == '.' || s[i - 1] == '!' || s[i - 1] == '?')
+		{
+			capital(&s[i]);
+		}
+		else if (s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{')
+		{
+			capital(&s[i]);
+		}
+		else if (s[i - 1] == ';' || s[i - 1] == '"' || s[i - 1] == '}')
+		{
+			capital(&s[i]);
+		}
+		else if (s[i - 1] == '\t')
+		{
+			capital(&s[i]);
 		}
 		else
 		{
@@ -31,7 +51,6 @@ char *cap_string(char *s)
 				s[i] = s[i] + 32;
 		}
 	}
-
-
 	return (s);
 }
+
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+*leet - encodes a string into 1337
+*@s: String value passed
+*Return: value
+**/
+char *leet(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] == 'a' )
+}

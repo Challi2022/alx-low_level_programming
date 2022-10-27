4e39668979ad10f11c75b2674c9e997c43e08abf
Chalachew Yisihak <chalachew.yisihak22@gmail.com>
2022-10-27 16:34:22 +0000

    Leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,7 +9,18 @@
 char *leet(char *s)
 {
 	int i;
+	char cmp[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char num[] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
 
-	for (i = 0; s[i] != '\0'; i++)
-		if (s[i] == 'a' )
+	i = 0;
+	while (s[i] != '\0')
+	{
+		for (i = 0; i < 10; i++)
+		{
+			if (s[i] == cmp[i])
+				*s = num[i];
+		}
+		i++;
+	}
+	return (s);
 }
